afd177bbbfcfc4814cccfe4a346fa757e7f0f8db
kovarrrski <mr.tonkonog@gmail.com>
2016-11-12 14:24:11 +0200

    3

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallom");
+            Console.WriteLine("Hallom;;;");
         }
     }
 }
